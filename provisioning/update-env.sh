9d57113efab85a4ba63dfe57cec738b6f3c9b39c
Elmar Atlason <elmar.atlason@gmail.com>
2017-12-07 19:26:51 +0000

    update-env no yaml

--- provisioning/update-env.sh
+++ provisioning/update-env.sh
@@ -26,7 +26,7 @@ done
 echo Status ${status}
 
 scp -o StrictHostKeyChecking=no -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem" ./ec2-instance-check.sh ec2-user@${INSTANCE_PUBLIC_NAME}:~/ec2-instance-check.sh
-scp -o StrictHostKeyChecking=no -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem" ./docker-compose.yaml ec2-user@${INSTANCE_PUBLIC_NAME}:~/docker-compose.yaml
+scp -o StrictHostKeyChecking=no -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem" ./docker-compose.yml ec2-user@${INSTANCE_PUBLIC_NAME}:~/docker-compose.yml
 scp -o StrictHostKeyChecking=no -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem" ./docker-compose-and-run.sh ec2-user@${INSTANCE_PUBLIC_NAME}:~/docker-compose-and-run.sh
 
 echo Wait for instance to be ready
